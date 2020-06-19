768d3576c042fa0d68e79963fbc45fd043de6484
fan <610398596@qq.com>
2020-06-19 11:45:25 +0800

    Direction implements turnleft and trunRight interface

--- src/main/java/com/twschool/practice/Direction.java
+++ src/main/java/com/twschool/practice/Direction.java
@@ -1,5 +1,53 @@
 package com.twschool.practice;
 
 public enum Direction {
-    N,W,S,E
+    N{
+        @Override
+        public void turnLeft(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("W");
+
+        }
+        @Override
+        public void turnRight(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("E");
+        }
+    }
+    ,W{
+        @Override
+        public void turnLeft(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("S");
+        }
+
+        @Override
+        public void turnRight(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("N");
+        }
+    }
+    ,S{
+        @Override
+        public void turnLeft(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("E");
+        }
+
+        @Override
+        public void turnRight(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("W");
+        }
+    }
+    ,E{
+        @Override
+        public void turnLeft(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("N");
+        }
+
+        @Override
+        public void turnRight(MarsRoverPosition marsRoverPosition) {
+            marsRoverPosition.setDirection("S");
+        }
+    };
+
+
+    public abstract void turnLeft(MarsRoverPosition marsRoverPosition);
+
+    public abstract void turnRight(MarsRoverPosition marsRoverPosition);
 }
