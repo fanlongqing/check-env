1aaa646c5790aff9c28b4534f60aee1fc6e65cff
fan <610398596@qq.com>
2020-06-20 11:41:51 +0800

    should_return_continued_when_compare_get_status_after_input_1243_6_given_game_with_answer_1234

--- src/main/java/com/twschool/practice/GameStatus.java
+++ src/main/java/com/twschool/practice/GameStatus.java
@@ -1,5 +1,5 @@
 package com.twschool.practice;
 
 public enum GameStatus {
-    SUCCED, CONTINUED;
+    SUCCED, CONTINUED, FAILD;
 }
--- src/main/java/com/twschool/practice/GuessNumberGame.java
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -1,17 +1,26 @@
 package com.twschool.practice;
 
 public class GuessNumberGame {
+    private static final int MAX_TRY_TIMES = 6;
     private final GameAnswer gameAnswer;
     private GameStatus gameStatus;
+    private int leftTryTimes=MAX_TRY_TIMES;
+
+
     public GuessNumberGame(GameAnswer gameAnswer) {
         this.gameAnswer=gameAnswer;
     }
 
     public String guess(String userAnswerString) {
         String result= gameAnswer.check(userAnswerString);
+        leftTryTimes--;
         if("4A0B".equals(result)){
              gameStatus = GameStatus.SUCCED;
-        }else {
+        }else if(leftTryTimes==0){
+            gameStatus=GameStatus.FAILD;
+        }
+        else
+            {
             gameStatus = GameStatus.CONTINUED;
         }
         return  result;
